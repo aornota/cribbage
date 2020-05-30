f1a1406213247473d719edf2f28936a02f76afc2
aornota <djnarration@gmail>
2020-05-30 21:06:48 +0100

    Work-in-progress.

--- src/domain/scoring.fs
+++ src/domain/scoring.fs
@@ -2,6 +2,17 @@ module Aornota.Cribbage.Domain.Scoring
 
 open Aornota.Cribbage.Domain.Core
 
+let private isRun (cards:Card list) =
+    match cards.Length with
+    | n when n > 2 ->
+        let ranks = cards |> List.map fst
+        if ranks |> List.distinct |> List.length = n then
+            let max, min = ranks |> List.max, ranks |> List.min
+            if (max.Value - min.Value) + 1 = n then Some (max, min)
+            else None
+        else None
+    | _ -> None
+
 type PeggingScoreEvent =
     private
     | PeggingPair of Rank
@@ -23,13 +34,11 @@ type PeggingScoreEvent =
             if runningTotal > MAX_PEGGING then failwithf "Cannot play %s when running total is %i" (cardText card) (int runningTotal)
             let rec findRun (cards:Card list) =
                 match cards.Length with
-                | n when n > 2 ->
-                    let ranks = cards |> List.map fst
-                    if ranks |> List.distinct |> List.length = n then
-                        let max, min = ranks |> List.max, ranks |> List.min
-                        if (max.Value - min.Value) + 1 = n then Some (max, min)
-                        else findRun (cards |> List.take (n - 1))
-                    else findRun (cards |> List.take (n - 1))
+                | n when n > 3 ->
+                    match isRun cards with
+                    | Some (high, low) -> Some (high, low)
+                    | None -> findRun (cards |> List.take (n - 1))
+                | 3 -> isRun cards
                 | _ -> None
             [
                 match current with
@@ -40,10 +49,10 @@ type PeggingScoreEvent =
                         | h :: _ when fst h = rank -> FourOfAKind rank
                         | _ -> ThreeOfAKind rank
                     | _ -> PeggingPair rank
-                | _ -> ()
-                match findRun (card :: current) with
-                | Some (high, low) -> PeggingRun (high, low)
-                | None -> ()
+                | _ ->
+                    match findRun (card :: current) with
+                    | Some (high, low) -> PeggingRun (high, low)
+                    | None -> ()
                 if runningTotal = 15<pip> then PeggingFifteen
                 else if runningTotal = MAX_PEGGING then ThirtyOne
             ]
@@ -71,13 +80,22 @@ type private CommonScoreEvent =
         if cards.Count <> 4 then failwithf "%s (%s) does not contain 4 Cards" (if isCrib then "Crib" else "Hand") (cardsText cards)
         else if cards.Contains cut then failwithf "%s (%s) must not contain cut Card (%s)" (if isCrib then "Crib" else "Hand") (cardsText cards) (cardText cut)
         let all = cut :: (cards |> List.ofSeq)
+        let distinctRanks = all |> List.map fst |> List.distinct |> List.length
         [
-            // TODO-NMB: Check for Fifteens | Pairs | Runs...
-
-            match all |> List.groupBy snd |> List.map fst with
-            | [ _ ] -> FiveFlush (all |> Set.ofList)
-            | _ -> ()
-            if fst cut <> Jack && all |> List.contains (Jack, snd cut) then Nob (Jack, snd cut)
+            // TODO-NMB: Fifteens...
+            if distinctRanks < 5 then
+                // TODO-NMB: Pairs...
+                ()
+            if distinctRanks > 2 then
+                // TODO-NMB: Runs (remember isRun above)...
+                ()
+            if distinctRanks = 5 then
+                match all |> List.groupBy snd |> List.map fst with
+                | [ _ ] -> FiveFlush (all |> Set.ofList)
+                | _ -> ()
+            if fst cut <> Jack then
+                let nob = Jack, snd cut
+                if cards.Contains nob then Nob nob
         ]
     member this.Score = (match this with | Fifteen _ | Pair _ -> 2 | Run cards -> cards.Count | FiveFlush _ -> 5 | Nob _ -> 1) * 1<point>
     member this.Text =
